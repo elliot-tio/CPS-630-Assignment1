b73ba9695b764568b8cde3156763fd1cec1ec4a4
Elliot Tio <etjinzo@gmail.com>
2018-01-27 21:40:28 -0500

    JavaScript now works - dropdown menus change div info based on selections.

--- assignment1-team20.js
+++ assignment1-team20.js
@@ -1,18 +1,59 @@
-var leftWindow = document.getElementById('left-window');
-var rightWindow = document.getElementById('right-window');
-var htmlContentLeft = '';
-var htmlContentRight = '';
+function populateWindow(sel){
+  var leftWindow = document.getElementById('left-window');
+  var rightWindow = document.getElementById('right-window');
+  var artists = document.getElementById('artists');
+  var htmlContentLeft = '';
+  var htmlContentRight = '';
 
-function populateWindow(event){
-  switch(event.target.value){
+  switch(sel.value){
     case 'artist1':{
-      htmlContentLeft = 'Content for A';
-      htmlContentRight = "content for A";
+      htmlContentLeft = 'artist 1';
+      htmlContentRight = "artist 1";
       break;
     }
     case 'artist2':{
-      htmlContentLeft = "content for B";
-      htmlContentRight = "content for B";
+      htmlContentLeft = "artist 2";
+      htmlContentRight = "artist 2";
+      break;
+    }
+    case 'artist3':{
+      htmlContentLeft = 'artist 3';
+      htmlContentRight = "artist 3";
+      break;
+    }
+    case 'artist4':{
+      htmlContentLeft = "artist 4";
+      htmlContentRight = "artist 4";
+      break;
+    }
+    case 'artist5':{
+      htmlContentLeft = 'artist 5';
+      htmlContentRight = "artist 5";
+      break;
+    }
+    case 'art1':{
+      htmlContentLeft = "art 1";
+      htmlContentRight = "art 1";
+      break;
+    }
+    case 'art2':{
+      htmlContentLeft = 'art 2';
+      htmlContentRight = "art 2";
+      break;
+    }
+    case 'art3':{
+      htmlContentLeft = "art 3";
+      htmlContentRight = "art 3";
+      break;
+    }
+    case 'art4':{
+      htmlContentLeft = 'art 4';
+      htmlContentRight = "art 4";
+      break;
+    }
+    case 'art5':{
+      htmlContentLeft = "art 5";
+      htmlContentRight = "art 5";
       break;
     }
   }
